27dcd7a17af8d6e448cb4aea464e9e287a5533cf
Harsha H M <harshahm472@gmail.com>
2025-10-15 17:10:58 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef, useEffect } from 'react';
+import { Chatbot } from 'supersimpledev';
+import RobotImage from './assets/robot.png';
+import UserImage from './assets/user.png';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+function ChatInput({chatMessages, setChatMessages}) {
+  const [inputText, setInputText]=useState('');
+
+  function saveInputText(event) {
+    setInputText(event.target.value);
+  }
+
+  function sendMessage() {
+    const newMessage = {message: inputText, sender: "user", id: Math.random()};
+    const newChatMessages = [...chatMessages, newMessage];
+    //setChatMessages(newChatMessages);
+
+    const response=Chatbot.getResponse(inputText);
+    console.log("Chatbot response: " + response);
+
+    const botMessage = {message: response, sender: "robot", id: Math.random()};
+    const newBotMessages = [...newChatMessages, botMessage];
+    setChatMessages(newBotMessages);
+    setInputText('');
+  }
+    return(
+      <div className="chat-input-container">
+        <input 
+          type="text" 
+          placeholder="Type your message..." 
+          size="32"
+          onChange={saveInputText}
+          value={inputText}
+          className="input-text"
+        />
+        <button onClick={sendMessage} className="send-button">Send</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    );
+  }
+      
+  function ChatOutput({message, sender}) {
+    //const message=props.message;
+    //const sender=props.sender;
+    //const {message, sender}=props;
+    /* if (sender==="robot") {
+      return(
+        <div>
+          <img src="robot.png" width="50"/>
+          {message}
+        </div>
+      );
+    } */
+
+    return(
+      <div className={sender === 'robot' ? "robot-message" : "user-message"}>
+        {sender === 'robot' && <img src={RobotImage} className="chat-message-profile"/>}
+        <div className="chat-message-text">{message}</div>
+        {sender === 'user' && <img src={UserImage} className="chat-message-profile"/>}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    );
+  }
+
+  function ChatMessages({chatMessages}){
+
+    const chatMessagesRef=useRef(null);
+    useEffect(() => {
+      const containerElem=chatMessagesRef.current;
+      if(containerElem){
+        containerElem.scrollTop=containerElem.scrollHeight;
+      }
+    }, [chatMessages]);
+
+    const chatMessageComponents=chatMessages.map((chatMessage) => {
+      return(<ChatOutput 
+        message={chatMessage.message} 
+        sender={chatMessage.sender}
+        key={chatMessage.id}
+      />);
+    });
+
+    return(
+      <div className="chat-messages-container" ref={chatMessagesRef}>
+        {chatMessageComponents}
+      </div>
+    );
+  }
+
+  function App(){
+        
+    const [chatMessages, setChatMessages] = useState([]);
+      //const [chatMessages, setChatMessages] = array;
+      //const chatMessages = array[0];
+      //const setChatMessages = array[1];
+      
+      return (
+        <div className="app-container">
+          <ChatMessages 
+            chatMessages={chatMessages} 
+          />
+          <ChatInput
+            chatMessages={chatMessages}
+            setChatMessages={setChatMessages}
+          />
+        </div>
+      );
+    }
 
 export default App
